8ba41007dbd211b149411781a1830e4fbcba7dba
marcusnk237 <74594432+marcusnk237@users.noreply.github.com>
2021-10-04 10:11:37 +0200

    Update auto_lock_door.ino

--- auto_lock_door.ino
+++ auto_lock_door.ino
@@ -1,11 +1,11 @@
-// Importation des bibliothèques 
-#include<Keypad.h> //Clavier
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h> //Ecran LCD
+// Loading libraires
+#include<Keypad.h> //Keypad
+#include <Wire.h> // Wire
+#include <LiquidCrystal_I2C.h> //LCD screen
 
 LiquidCrystal_I2C lcd(0x3F,20,4);
 
-// Caractères du clavier
+// Keypad characters
 char keys[4][3]={
  {'1','2','3'},
  {'4','5','6'},
@@ -15,83 +15,83 @@ char keys[4][3]={
 byte rowPin[4]={6,7,8,9};
 byte colPin[3]={3,4,5};
 
-String password = "1234";  // Le mot de passe. Il peut être modifié 
-int position = 0; // Pointeur indiquant la position du caractère saisie dans le mot de passe
+String password = "1234";  // Password 
+int position = 0; // Character's password positiion
 
-int wrong = 0; // Variable permettant de calculer le nombre de tentatives ratées
+int wrong = 0; // Failed attempt counter
 
-int redPin = 10; // Sortie indiquant que la porte est bloquée
-int greenPin = 11; // Sortie indiquant que la porte est ouverte
-int buzzer = 12; // Sortie de l'alarme
- 
+int redPin = 10; // Locked door output
+int greenPin = 11; // Opened door output
+int buzzer = 12; // Alarm output
+
+// Keypad mapping 
 Keypad keypad=Keypad(makeKeymap(keys),rowPin,colPin,4,3);
-// Mapping du clavier
 
-int total = 0; // nombre de tentatives ratées totale
-// Initialisation 
+int total = 0; // Failed attempt overall
+// Initialization 
 void setup(){
   
  pinMode(redPin,OUTPUT);
  pinMode(greenPin,OUTPUT);
  pinMode(buzzer, OUTPUT);
- lcd.init(); //Initialisation du lcd
+ lcd.init(); //LCD initialization
  lcd.backlight();
- lcd.clear(); // Reinitialisation de l'écran
- setLocked(true); // Verrouillage
+ lcd.clear(); // Clear LCD screen
+ setLocked(true); // Locking
  delay(20);
 }
 
 void loop(){
   
-  lcd.clear(); // Reinitialisation de l'écran
-  lcd.print(" Entrer le code : ");
+  lcd.clear(); // Clear LCD screen
+  lcd.print(" Enter the code : ");
   delay(100);
-  char pressed=keypad.getKey();//Récuperation de la touche pressée
+  char pressed=keypad.getKey();//Get the inpur
   String key[3];
   
   if(pressed){
-    if(pressed == '#'){ // Verrouillage à l'appui de la touche #
-          position = 0; // Reinitilisation du pointeur
-          setLocked(true); // Verrouillage
-          lcd.clear(); // Reinitialisation de l'écran
+    if(pressed == '#'){ // The door is lock if we press '#'
+          position = 0; // Clear character's password position
+          setLocked(true); // Locked
+          lcd.clear(); // Clear LCD screen
       }
 
-     else if(pressed == password[position]){ // Le caractère pressé est dans le bon ordre 
+     else if(pressed == password[position]){ //The character is in the right order 
           key[position]=pressed;
-          position++; // Incrémentation du pointeur
+          position++; // Incrementation of the position
       }
  
-     else if (pressed != password[position] ) // Le caractère n'est pas dans le bon ordre
+     else if (pressed != password[position] ) // The character is in the wrong order 
       {
-          wrong++; // Incrémentation du nombre d'échecs
-          position ++; // Incrémentation du pointeur
+          wrong++; // Incrementation of failed attempt counter
+          position ++; // Incrementation of the position
       }
 
     if(position == 4){
-          if( wrong >0) // Le mot de passe inséré n'est pas le bon
+          if( wrong >0) // The password is wrong
             {
                 
-                total++; // Incrémentation du nombre global d'echecs
-                wrong = 0; // Reinitialisation du compteur
-                position = 0; // Reinitilisation du pointeur
-                lcd.clear(); // Reinitialisation de l'écran LCD
+                total++; // Incrementation of the overall failed attempt
+                wrong = 0; // clear counter
+                position = 0; // Clear character's password position
+                lcd.clear(); //  Clear LCD screen 
                 lcd.setCursor(0,1);
-                lcd.print("  CODE ERRONE!");
+                lcd.print("  WRONG CODE!");
                 lcd.setCursor(0,2);
-                lcd.print("  ESSAIS RESTANTS: ");
+                lcd.print("  LEFT ATTEMPTS: ");
                 lcd.setCursor(19,2);
                 lcd.print(3-total);
                 setLocked(true); // Vérouillage
             }
 
-          else if(position == 4 && wrong == 0) // Le mot de passe inséré est le bon
+          else if(position == 4 && wrong == 0) // The code is right
             {
-                buzzer_beep_unlock(); // Dévérouillage
+                buzzer_beep_unlock(); // Unlock the door
             }
 
-             if(total ==3){ // 3 Echecs successifs
-                total=0;  // Reinitialisation du nombre total d'échecs
-                buzzer_beep_lock(); // Déclenchement de l'alarme
+             if(total ==3){ // 3 failed attempt in a row
+                total=0;  // clear counter
+                buzzer_beep_lock(); // Start the alarm
             }
 
         }
@@ -101,39 +101,39 @@ void loop(){
    
 }
 
-void setLocked(int locked){ // Fonction de vérouillage/dévérouillage de la porte
-    if (locked){ // vérouillage
-          digitalWrite(redPin, HIGH); // LED de vérouillage à 1
-          digitalWrite(greenPin, LOW); // LED de dévérouillage à 0
+void setLocked(int locked){ // Lock/unlock function
+    if (locked){ // Lock
+          digitalWrite(redPin, HIGH); // Set Lock LED to  1
+          digitalWrite(greenPin, LOW); // Set Unlock LED to  0
           delay(2000);
       }
-    else{ //dévérouillage
-          digitalWrite(redPin, LOW); // LED de vérouillage à 0
-          digitalWrite(greenPin, HIGH); // LED de dévérouillage à 1
+    else{ //Unlock
+          digitalWrite(redPin, LOW); // Set Lock LED to  0
+          digitalWrite(greenPin, HIGH); //Set Unlock LED to  1
           delay(5000);
-          digitalWrite(redPin, HIGH); // LED de vérouillage à 1
-          digitalWrite(greenPin, LOW); // LED de dévérouillage à 0
+          digitalWrite(redPin, HIGH); // Set Lock LED to  1
+          digitalWrite(greenPin, LOW); // Set Unlock LED to  0
       }
   }
-void buzzer_beep_lock(){ // Fonction alarme
-   lcd.clear(); // Reinitialisation de l'écran
+void buzzer_beep_lock(){ // Alarm
+   lcd.clear(); // Clear LCD Screen
    lcd.setCursor(0,1);
-   lcd.print("   ACCES REFUSE !!!");
-   // Déclenchement de l'alarme pendant 10 secondes
+   lcd.print("   ACCESS DENIED !!!");
+   // Start alarm during 10 seconds
    digitalWrite(buzzer,HIGH);
    delay(10000);
    digitalWrite(buzzer,LOW);
    
 }
-void buzzer_beep_unlock(){ // Fonction alarme
-   position = 0; // Reinitilisation du pointeur
-   wrong = 0; // Reinitialisation du compteur
-   lcd.clear(); // Reinitialisation de l'écran LCD
+void buzzer_beep_unlock(){ // Alarm
+   position = 0; // Clear character's password position
+   wrong = 0; // Clear counter
+   lcd.clear(); // Clear LCD Screen
    lcd.setCursor(0,1);
-   lcd.print("      BIENVENUE!     ");
+   lcd.print("      WELCOME!     ");
    digitalWrite(buzzer,HIGH);
    delay(100);
    digitalWrite(buzzer,LOW);
-   setLocked(false); // Dévérouillage
+   setLocked(false); // unlock
    
 }
